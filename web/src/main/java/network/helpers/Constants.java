f02f52f871a90973fefbaa51cf42ba10895ee781
Roman <chevron19941@gmail.com>
2017-02-02 09:46:42 +0300

    Убран хардкод констант(берутся из системы)

--- web/src/main/java/network/helpers/Constants.java
+++ web/src/main/java/network/helpers/Constants.java
@@ -4,9 +4,17 @@ package network.helpers;
  * Created by Роман on 30.01.2017.
  */
 public class Constants {
-    public static final String HOST_URL = "https://img.hello-from.tk/";
-    public static final String FILES_PATH = "/home/roman/hellofrom";
-    public static final String EMAIL_LOGIN = "no-reply@hello-from.tk";
-    public static final String EMAIL_PASSWORD = "Chevron94Vrn";
-    public static final String SMTP_HOST = "smtp.yandex.ru";
+    public static final String HOST_URL;// = "https://img.hello-from.tk/";
+    public static final String FILES_PATH;// = "/home/roman/hellofrom";
+    public static final String EMAIL_LOGIN;// = "no-reply@hello-from.tk";
+    public static final String EMAIL_PASSWORD;// = "Chevron94Vrn";
+    public static final String SMTP_HOST;// = "smtp.yandex.ru";
+
+    static {
+        HOST_URL = System.getProperty("HOST_URL");
+        FILES_PATH = System.getProperty("FILES_PATH");
+        EMAIL_LOGIN = System.getProperty("EMAIL_LOGIN");
+        EMAIL_PASSWORD = System.getProperty("EMAIL_PASSWORD");
+        SMTP_HOST = System.getProperty("SMTP_HOST");
+    }
 }
